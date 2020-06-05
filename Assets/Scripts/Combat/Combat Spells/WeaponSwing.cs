75363157f2372c7c2b2b322daad0ec50ebcc0b5e
IvanNSBS <insbs@cin.ufpe.br>
2020-06-05 11:22:21 -0300

    Add weapon Swing

---
+++ Assets/Scripts/Combat/Combat Spells/WeaponSwing.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+[CreateAssetMenu(menuName = "Spells/Weapon Swing")]
+public class WeaponSwing : SpellObject
+{
+    [Header("Damage")]
+    [Range(0, 5)] public float weaponDmgIncreaseInPct = 1.7f;
+
+    [Header("FX Properties")]
+    public float duration = 1f;
+    public Vector3 spawnOffset = Vector3.zero;
+
+    private GameObject instantiatedFX;
+    public override void Cast(GameObject caster){
+
+        CombatAttacker _casterAtk = caster.GetComponent<CombatAttacker>();
+        if(!_casterAtk)
+            return;
+
+        var fx = Instantiate(instantiatedGO);
+        fx.transform.position = caster.transform.position + spawnOffset;
+
+        var dmgData = fx.GetComponent<DamageData>();
+        var dmgOnCol = fx.GetComponent<DamageOnCollide>();
+        var destroy = fx.GetComponent<DestroyAfterTime>();
+        
+        destroy.duration = duration;
+        destroy.remainingTime = duration;
+
+        dmgData.damage = _casterAtk.weapon.atkDamage * weaponDmgIncreaseInPct;
+        dmgData.caster = caster;
+
+        dmgOnCol.dmgData = dmgData;
+
+        if(spellSound)
+            GameplayUtils.SpawnSound(spellSound, caster.transform.position);
+    }
+}
