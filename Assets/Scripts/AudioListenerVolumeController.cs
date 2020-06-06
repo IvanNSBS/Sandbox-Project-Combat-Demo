327f2004685c433e68765087d816c27a8d332c19
IvanNSBS <insbs@cin.ufpe.br>
2020-06-06 15:43:10 -0300

    Add SFX Volume Control

---
+++ Assets/Scripts/AudioListenerVolumeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class AudioListenerVolumeController : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private SliderData _data;
+
+    private void OnEnable()
+    {
+        _slider.value = _data.volume;
+        AudioListener.volume = _data.volume;
+
+        _slider.onValueChanged.AddListener((val) => {
+            _data.volume = val;
+            AudioListener.volume = _data.volume;
+        });
+    }
+
+    private void OnDisable() {
+        _slider.onValueChanged.RemoveAllListeners();    
+    }
+}
---
+++ Assets/Scripts/SliderData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Volume/Master Volue Data")]
+public class SliderData : ScriptableObject
+{
+    [Range(0f, 1f)]public float volume = 0.75f; 
+}
